75fc94239cedefbd73b11d6028b2c0f141ea23c8
parkhaemi <haemipark21@gmail.com>
2021-07-07 03:36:54 +0900

    210706:haemi

--- app/src/main/java/com/example/taveproject0701/ChatbotActivity.java
+++ app/src/main/java/com/example/taveproject0701/ChatbotActivity.java
@@ -11,21 +11,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jetbrains.annotations.NotNull;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class ChatbotActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chatbot);
 
         //Initialize and Assign Variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
 
         //Set Home Selected
-        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+        bottomNavigationView.setSelectedItemId(R.id.menu_chatbot);
 
         //Perform ItemSelectedListner
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -33,11 +31,11 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.menu_chatbot:
-                        startActivity(new Intent(getApplicationContext(), ChatbotActivity.class ));
-                        overridePendingTransition(0,0);
                         return true;
 
                     case R.id.menu_home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class ));
+                        overridePendingTransition(0,0);
                         return true;
 
                     case R.id.menu_map:
--- app/src/main/java/com/example/taveproject0701/MapActivity.java
+++ app/src/main/java/com/example/taveproject0701/MapActivity.java
@@ -11,21 +11,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jetbrains.annotations.NotNull;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class MapActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
 
         //Initialize and Assign Variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
 
         //Set Home Selected
-        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+        bottomNavigationView.setSelectedItemId(R.id.menu_map);
 
         //Perform ItemSelectedListner
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,17 +36,19 @@ public class MainActivity extends AppCompatActivity {
                         return true;
 
                     case R.id.menu_home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class ));
+                        overridePendingTransition(0,0);
                         return true;
 
                     case R.id.menu_map:
-                        startActivity(new Intent(getApplicationContext(), MapActivity.class ));
-                        overridePendingTransition(0,0);
+
                         return true;
 
                 }
 
                 return false;
             }
+
         });
     }
 }
